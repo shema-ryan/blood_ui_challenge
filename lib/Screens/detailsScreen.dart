0daad425b77ee920a0598a41e9db9e6317ff854d
shema-ryan <kamanzishema@gmail.com>
2021-05-21 17:35:28 +0300

    fixed an overflow caused The SoftKeyBoard

--- lib/Screens/detailsScreen.dart
+++ lib/Screens/detailsScreen.dart
@@ -14,144 +14,130 @@ class DetailScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(_text),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            height: _size * 0.9923,
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('A BIT ABOUT YOU' , style: _theme.textTheme.headline6.copyWith(
-                  color: Constants.textColor
-                ),),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('NAME' , style:_theme.textTheme.headline6,),
-                        SizedBox(width : _size * 0.6361 ,child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: _size * 0.9923,
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('A BIT ABOUT YOU' , style: _theme.textTheme.headline6.copyWith(
+                    color: Constants.textColor
+                  ),),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('NAME' , style:_theme.textTheme.headline6,),
+                          SizedBox(width : _size * 0.6361 ,child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none
+                              ),
+                              filled: true ,
+                              fillColor: Constants.fillColor.withOpacity(0.2),
+                              hintText: 'Your First Name',
                             ),
-                            filled: true ,
-                            fillColor: Constants.fillColor.withOpacity(0.2),
-                            hintText: 'Your First Name',
-                          ),
-                        ),)
-                      ],
-                    ),
-                    const SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('CONTACT' , style:_theme.textTheme.headline6,),
-                        SizedBox(width : _size * 0.6361 , child: TextField(
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none
+                          ),)
+                        ],
+                      ),
+                      const SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('CONTACT' , style:_theme.textTheme.headline6,),
+                          SizedBox(width : _size * 0.6361 , child: TextField(
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none
+                              ),
+                              filled: true ,
+                              fillColor: Constants.fillColor.withOpacity(0.2),
+                              hintText: '10 Digit Phone No',
                             ),
-                            filled: true ,
-                            fillColor: Constants.fillColor.withOpacity(0.2),
-                            hintText: '10 Digit Phone No',
-                          ),
-                        ),)
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('STATE' , style:_theme.textTheme.headline6,),
-                        SizedBox(width : _size * 0.6361 ,child: DropdownButtonFormField(
-                          onChanged: (value ){},
-                          items: [
-                            DropdownMenuItem(child: Text('kampala'), value: 1,),
-                            DropdownMenuItem(child: Text('kigali') , value: 2,),
-                          ],
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none
-                            ),
-                            filled: true ,
-                            fillColor: Constants.fillColor.withOpacity(0.2),
-                            hintText: 'Select Your State',
-                          ),
-                        ),)
-                      ],
-                    ),
-                    const SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('DISTRICT' , style:_theme.textTheme.headline6,),
-                        SizedBox(width : _size * 0.6361 , child: DropdownButtonFormField(
-                          onChanged: (_){},
-                          items: [
-                            DropdownMenuItem(child: Text('kampala'), value: 1,),
-                            DropdownMenuItem(child: Text('kigali') , value: 2,),
-                          ],
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none
-                            ),
-                            filled: true ,
-                            fillColor: Constants.fillColor.withOpacity(0.2),
-                            hintText: 'Select Your District',
-                          ),
-                        ),)
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: _size * 0.5089,
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(10),
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Your Age?' , style: _theme.textTheme.headline6,),
-                Expanded(child:DropdownButtonFormField(
-                  onChanged: (value){},
-                  items: [
-                    DropdownMenuItem(child: Text('kampala'), value: 1,),
-                    DropdownMenuItem(child: Text('kigali') , value: 2,),
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none
-                    ),
-                    filled: true ,
-                    fillColor: Constants.fillColor.withOpacity(0.2),
-                    hintText: 'Select Your Age',
+                          ),)
+                        ],
+                      ),
+                    ],
                   ),
-                ),),
-                Text('What\'s Your Blood Group ?' , style: _theme.textTheme.headline6,),
-                Expanded(
-                  child: DropdownButtonFormField(
-                    onChanged: (value){
-
-                    },
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('STATE' , style:_theme.textTheme.headline6,),
+                          SizedBox(width : _size * 0.6361 ,child: DropdownButtonFormField(
+                            onChanged: (value ){},
+                            items: [
+                              DropdownMenuItem(child: Text('kampala'), value: 1,),
+                              DropdownMenuItem(child: Text('kigali') , value: 2,),
+                            ],
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none
+                              ),
+                              filled: true ,
+                              fillColor: Constants.fillColor.withOpacity(0.2),
+                              hintText: 'Select Your State',
+                            ),
+                          ),)
+                        ],
+                      ),
+                      const SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('DISTRICT' , style:_theme.textTheme.headline6,),
+                          SizedBox(width : _size * 0.6361 , child: DropdownButtonFormField(
+                            onChanged: (_){},
+                            items: [
+                              DropdownMenuItem(child: Text('kampala'), value: 1,),
+                              DropdownMenuItem(child: Text('kigali') , value: 2,),
+                            ],
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none
+                              ),
+                              filled: true ,
+                              fillColor: Constants.fillColor.withOpacity(0.2),
+                              hintText: 'Select Your District',
+                            ),
+                          ),)
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: _size * 0.5089,
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Your Age?' , style: _theme.textTheme.headline6,),
+                  Expanded(child:DropdownButtonFormField(
+                    onChanged: (value){},
                     items: [
                       DropdownMenuItem(child: Text('kampala'), value: 1,),
                       DropdownMenuItem(child: Text('kigali') , value: 2,),
@@ -163,23 +149,44 @@ class DetailScreen extends StatelessWidget {
                       ),
                       filled: true ,
                       fillColor: Constants.fillColor.withOpacity(0.2),
-                      hintText: 'Select Your Blood Group',
+                      hintText: 'Select Your Age',
+                    ),
+                  ),),
+                  Text('What\'s Your Blood Group ?' , style: _theme.textTheme.headline6,),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      onChanged: (value){
+
+                      },
+                      items: [
+                        DropdownMenuItem(child: Text('kampala'), value: 1,),
+                        DropdownMenuItem(child: Text('kigali') , value: 2,),
+                      ],
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none
+                        ),
+                        filled: true ,
+                        fillColor: Constants.fillColor.withOpacity(0.2),
+                        hintText: 'Select Your Blood Group',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                backgroundColor:  Constants.iconButtonColor,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                minimumSize:Size(_size * 0.8906 , _size* 0.1017),
+                ],
               ),
-              onPressed: ()=>print('hello shema'), child: Text('Submit' , style: _theme.textTheme.headline6.copyWith(
-              color: Colors.white
-          ),))
-        ],
+            ),
+            OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor:  Constants.iconButtonColor,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                  minimumSize:Size(_size * 0.8906 , _size* 0.1017),
+                ),
+                onPressed: ()=>print('hello shema'), child: Text('Submit' , style: _theme.textTheme.headline6.copyWith(
+                color: Colors.white
+            ),))
+          ],
+        ),
       ),
     );
   }
